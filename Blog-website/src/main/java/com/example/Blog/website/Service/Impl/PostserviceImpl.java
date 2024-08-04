b82f116db831e92dacad2fc822e1316d2fb62993
yash kaushik <152710350+yashkaushik9555@users.noreply.github.com>
2024-08-04 19:48:54 +0530

    Update PostserviceImpl.java

--- Blog-website/src/main/java/com/example/Blog/website/Service/Impl/PostserviceImpl.java
+++ Blog-website/src/main/java/com/example/Blog/website/Service/Impl/PostserviceImpl.java
@@ -85,6 +85,7 @@ public class PostserviceImpl implements PostService {
 			postFindById.setDescription(postDto.getDescription());
 			postFindById.setIsActive(postDto.getIsActive());
 			postFindById.setTitle(postDto.getTitle());
+			postFindById.setImageName(postDto.getImageName());
 			Posts save = this.postRespo.save(postFindById);
 			return new ResponseEntity<ResponseUtil>(new ResponseUtil(APPConstant.SUCCESS_MESSAGE,
 					APPConstant.SUCCESS_MESSAGE, this.modelMappper.map(save, PostDto.class), HttpStatus.OK),
